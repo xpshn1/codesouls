76d98f8b25c1c92cc7ce684a379838835fd93c19
Kalyan Molugooru <kalyanmolugooru@gmail.com>
2026-05-07 22:14:10 -0400

    Apply themed typography system

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/geist-sans/400.css'
+import '@fontsource/geist-sans/500.css'
+import '@fontsource/geist-sans/600.css'
+import '@fontsource/geist-sans/700.css'
+import 'typeface-hk-grotesk/index.css'
 import './index.css'
 import App from './App.tsx'
 
